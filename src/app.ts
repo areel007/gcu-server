87113250d17fab2860e885dfbe767c2059ec6767
bamidele <morenikejisunday12@gmail.com>
2025-11-13 21:44:39 +0100

    fix cors bug

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,20 @@ import routes from "./routes";
 const app = express();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173", // ✅ dev
-    "https://gcu-mobile.surge.sh", // ✅ deployed frontend
-  ],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: ["http://localhost:5173", "https://gcu-mobile.surge.sh"],
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 204,
-  allowedHeaders: "Content-Type, Authorization",
 };
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // IMPORTANT
 
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors(corsOptions));
+
 app.use("/api", routes);
 
 export default app;
